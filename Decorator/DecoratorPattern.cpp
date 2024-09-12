59d64c92539edf691f7ccc8c3beb4034526e71e2
abhaysrivastav <abhay.ercomp@gmail.com>
2024-09-12 13:21:39 +0530

    Update DecoratorPattern.cpp

--- Decorator/DecoratorPattern.cpp
+++ Decorator/DecoratorPattern.cpp
@@ -1,1 +1,12 @@
+int main()
+{
+  VisualComponent* textView = new TextView(); 
+  VisualComponent* scrollTextView = new ScrollDecorator(textView);
+  VisualComponent* borderScrollTextView = new BorderDecorator(scrollTextView);
 
+  delete textView;
+  delete scrollableTextView;
+  delete borderedScrollableTextView;
+
+  return 0;
+}
