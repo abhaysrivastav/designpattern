a8e3a3151250006f974ae507745d3d20d5e0635f
abhaysrivastav <abhay.ercomp@gmail.com>
2024-09-12 13:12:20 +0530

    Update ScrollDecorator.h

--- Decorator/ScrollDecorator.h
+++ Decorator/ScrollDecorator.h
@@ -1,1 +1,13 @@
+class ScrollDecorator : public Decorator {
+public:
+    ScrollDecorator(VisualComponent* comp) : Decorator(comp) {}
 
+    void draw() override {
+        Decorator::draw();  // Call the base component's draw method
+        drawScrollBar();    // Add scrollbar-related functionality
+    }
+
+    void drawScrollBar() {
+        std::cout << "Drawing scrollbar.\n";
+    }
+};
