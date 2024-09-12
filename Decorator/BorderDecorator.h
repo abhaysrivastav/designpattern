fe75115eb227bc1c03fab2ab3e9079f6862919ef
abhaysrivastav <abhay.ercomp@gmail.com>
2024-09-12 13:11:05 +0530

    Update BorderDecorator.h

--- Decorator/BorderDecorator.h
+++ Decorator/BorderDecorator.h
@@ -1,1 +1,16 @@
-
+class BorderDecorator : public Decorator
+{
+private:
+  int borderWidth; 
+public:
+  BorderDecorator(VisualComponent* comp, int width):  Decorator(comp), borderWidth(width) {}
+  void draw() override
+  {  
+    Decorator::draw();
+    drawBorder(); 
+  }
+  void drawBorder()
+  {
+    std::cout << "Drawing Border" << borderWidth << std::endl;
+  }  
+};
